55fe02209fb80bb7d071a8b13caf9ce36ad15343
Opilane TTHK <opilane@tthk.ee>
2020-09-01 13:20:00 +0300

    Create My First app

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //this is a commentary
+            // Console.WriteLine("Hello Janika!");
+            /* 1. The program ask the user´s name 
+             * 2. The user enters their name 
+             * 3. The program greets the user by their name*/
+            Console.WriteLine("What`s your name?");
+            string Name;
+            Name = Console.ReadLine();
+            Console.WriteLine("Hello," + Name);
+
+
+
+        }
+    }
+}
